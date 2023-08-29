ce3b8062cc02256eb4ba1ebfed135f6fe26352ab
alpheratz0 <alpheratz99@protonmail.com>
2023-08-29 18:49:35 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # Copyright (C) 2022 <alpheratz99@protonmail.com>
 # This program is free software.
 
-AR        = ar
-CC        = cc
-CFLAGS    = -ansi -pedantic -Wall -Wextra -Os
-PREFIX    = /usr/local
+AR=ar
+CC=cc
+CFLAGS=-ansi -pedantic -Wall -Wextra -Os
+PREFIX=/usr/local
